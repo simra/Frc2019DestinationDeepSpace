c4692d41797d3bb93a3b3fcbc4c8d81f01099937
Robert Sim <rsim@microsoft.com>
2019-02-09 17:40:16 -0800

    Homography for more accurate line following. [untested]

--- robot/src/main/java/team492/LineFollowingUtils.java
+++ robot/src/main/java/team492/LineFollowingUtils.java
@@ -1,6 +1,13 @@
 package team492;
 
-// import org.opencv.
+import org.opencv.core.Core;
+import org.opencv.core.Point;
+import org.opencv.core.Size;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.calib3d.*;
+import java.util.LinkedList;
 
 public class LineFollowingUtils
 {
@@ -55,6 +62,67 @@ public class LineFollowingUtils
         public double getYLength()
         {
             return yLength;
+        }
+    }
+
+    public static class CameraFieldOfView
+    {
+        // X,Y robot coordinates of the corners of the image.  we're assuming no lens distortion
+        private Point topLeft;
+        private Point topRight;
+        private Point bottomLeft;
+        private Point bottomRight;
+        private double xResolution;
+        private double yResolution;
+        // homography matrix mapping image corners to real-world coordinates.
+        private Mat H;
+
+        public CameraFieldOfView(
+            Point topLeft,
+            Point topRight,
+            Point bottomLeft,
+            Point bottomRight,
+            double xResolution,
+            double yResolution
+        ) {
+            this.topLeft = topLeft;
+            this.topRight = topRight;
+            this.bottomLeft = bottomLeft;
+            this.bottomRight = bottomRight;
+            // find the 3x3 homography matrix H 
+            LinkedList<Point> objList = new LinkedList<Point>();
+            LinkedList<Point> sceneList = new LinkedList<Point>();
+
+            objList.add(topLeft);
+            sceneList.add(new Point(0,0));
+            objList.add(topRight);
+            sceneList.add(new Point(this.xResolution, 0));
+            objList.add(bottomLeft);
+            sceneList.add(new Point(0, this.yResolution));
+            objList.add(bottomRight);
+            sceneList.add(new Point(this.xResolution, this.yResolution));
+
+            MatOfPoint2f obj = new MatOfPoint2f();
+            obj.fromList(objList);
+
+            MatOfPoint2f scene = new MatOfPoint2f();
+            scene.fromList(sceneList);
+
+            // find the homography from scene (image coords) to obj (world coords).
+            // note this relative to a scale factor. hopefully s=1 will Just Work.
+            this.H = Calib3d.findHomography(scene, obj);
+        }
+
+        // get the real world coordinates of a image (x,y) point.
+        public Point GetRealWorldCoords(Point p) 
+        {
+            // todo: verify
+            Mat pMat = Mat.ones(new Size(3,1), CvType.CV_32F);
+            pMat.put(0, 0, p.x);
+            pMat.put(1, 0, p.y);
+            Mat result = new Mat();
+            Core.gemm(this.H, pMat, 1.0, new Mat(), 0, result);
+            return new Point(result.get(0, 0)[0],result.get(0,1)[0]); // todo: maybe divide by result.get(0,2)
         }
     }
 }
